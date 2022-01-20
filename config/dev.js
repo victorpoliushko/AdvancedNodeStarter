b29fbb7e2f68f1d3c073c63b0dfac31088c8ee35
viktor poliushko <viktor.poliushko@workrise.com>
2022-01-20 23:52:51 +0200

    caching the data, promisify

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,6 @@ module.exports = {
   googleClientID:
     '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
   googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb://admin:admin@cluster0-shard-00-00.ty6uc.mongodb.net:27017,cluster0-shard-00-01.ty6uc.mongodb.net:27017,cluster0-shard-00-02.ty6uc.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-wv6dv6-shard-0&authSource=admin&retryWrites=true&w=majority',
   cookieKey: '123123123',
 };
