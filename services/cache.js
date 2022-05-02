5e5baf8072a115b8cabbf021c6f1d8b5fe6ee394
viktor poliushko <viktor.poliushko@workrise.com>
2022-05-03 00:52:26 +0300

    redis cache

--- services/cache.js
+++ services/cache.js
@@ -1,12 +1,30 @@
 const mongoose = require('mongoose');
+const redis = require('redis');
+const util = require('util');
 
+const redisUrl = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+client.get = util.promisify(client.get);
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = function() {
-  console.log('I am about to run a query');
+mongoose.Query.prototype.exec = async function() {
+  const key = JSON.stringify(
+    Object.assign({}, this.getQuery(), {
+      collection: this.mongooseCollection.name
+    })
+  );
 
-  console.log(this.getQuery());
-  console.log(this.mongooseCollection.name);
+  const cacheValue = await client.get(key);
 
-  return exec.apply(this, arguments);
+  if(cacheValue) {
+    const doc = new this.model(JSON.parse(cacheValue));
+
+    return doc;
+  }
+
+  const result = await exec.apply(this, arguments);
+
+  client.set(key, JSON.stringify(result));
+  
+  return result;
 }
