c376f8163988eefd1b9d2e78cae796f4d7468528
viktor-poliushko-sombra <106060414+viktor-poliushko-sombra@users.noreply.github.com>
2022-05-31 01:55:38 +0300

    cache improvement

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ if (['production'].includes(process.env.NODE_ENV)) {
   });
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Listening on port`, PORT);
 });
--- services/cache.js
+++ services/cache.js
@@ -7,7 +7,16 @@ const client = redis.createClient(redisUrl);
 client.get = util.promisify(client.get);
 const exec = mongoose.Query.prototype.exec;
 
+mongoose.Query.prototype.cache = function() {
+  this.useCache = true;
+  return this;
+}
+
 mongoose.Query.prototype.exec = async function() {
+  if (!this.useCache) {
+    return exec.apply(this, arguments);
+  }
+
   const key = JSON.stringify(
     Object.assign({}, this.getQuery(), {
       collection: this.mongooseCollection.name
@@ -16,15 +25,17 @@ mongoose.Query.prototype.exec = async function() {
 
   const cacheValue = await client.get(key);
 
-  if(cacheValue) {
-    const doc = new this.model(JSON.parse(cacheValue));
+  if (cacheValue) {
+    const doc = JSON.parse(cacheValue);
 
-    return doc;
+    return Array.isArray(doc)
+      ? doc.map(d => new this.model(d))
+      : new this.model(doc);
   }
 
   const result = await exec.apply(this, arguments);
 
-  client.set(key, JSON.stringify(result));
+  client.set(key, JSON.stringify(result), 'EX', 10);
   
   return result;
 }
